0a9af2afd0dfe6fe33e6a7941447f683c03365fa
Wingwo <wingwo.kwai@hackney.gov.uk>
2019-05-17 16:00:42 +0100

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+// Add your routes here - above the module.exports line
+
+router.post("/juggling-balls-answer", function(request, response){
+
+    var jugglingBalls = request.session.data['juggling-balls']
+
+    if (jugglingBalls == "3 or more"){
+response.redirect("/juggling-tricks")
+    } else {
+        response.redirect("/ineligible")
+    }
+
+})
+
+module.exports = router
